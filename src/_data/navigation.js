1645ff1bb92e76ed64f4378bc248fbb48f70db68
Mike Mathew <m2mathew@me.com>
2026-01-04 18:00:08 -0600

    refactor: update header/footer component

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -4,35 +4,5 @@ export default {
       text: 'About',
       url: '/about/'
     },
-    {
-      text: 'Docs',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
-      text: 'Blog',
-      url: '/blog/'
-    }
   ],
-  bottom: [
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    },
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    },
-    {
-      text: 'Accessibility',
-      url: '/accessibility/'
-    }
-  ]
 };
